735542a0509fbe4d77fde6d58c2b9cea1a3e1179
JeffCEmploynet <jcampbell@employnet.com>
2024-03-22 11:19:02 -0700

    list results of employee search

--- src/app/components/cards/SearchResult.tsx
+++ src/app/components/cards/SearchResult.tsx
@@ -1,20 +1,39 @@
 import Link from "next/link";
+import BlueCard from "./BlueCard";
 
-export default function SearchResult(
-  {id, name, url, count} : {id: any, name: any, url: string, count: number}
+export function SearchResult(
+  {id, lastName, firstName, branch, url} : {id: any, lastName: string, firstName: string, branch: string, url: string}
 ){
+  return (
+    <Link href={url}>
+      <div className="flex justify-between border m-1 border-white p-1 hover:bg-white">
+        <p>{id}</p>
+        <p>{lastName}</p>
+        <p>{firstName}</p>
+        <p>{branch}</p>
+      </div>
+    </Link>
+  )
+}
 
-  let resultList = [];
+export function ResultsDiv({searchResultList} : {searchResultList: Array<any>}){
+  const allResults = searchResultList.map((result, index)=>
+    <div key={index}>{result}</div>
+  );
 
-  for (var i = 0; i < count; i++){
-    resultList.push(
-      <div className="flex">
-        <Link href={url}>
-          <p>{name}</p>
-          <p>{id}</p>
-        </Link>
-      </div>)
-  }
+  console.log(allResults);
 
-  return resultList;
+  return(
+    <BlueCard
+      content={
+        <ul className="flex flex-col w-full">
+          <div className="flex justify-between m-1 p-2 bg-white">
+            <p>Name</p>
+            <p>Id</p>
+          </div>
+          {allResults}
+        </ul>
+      }
+    />
+  )
 }
--- src/app/employee/search/page.tsx
+++ src/app/employee/search/page.tsx
@@ -1,50 +1,71 @@
 'use client'
-import { useState, useEffect } from "react";
+import { useState, useEffect, use } from "react";
 import BlueCard from "@/app/components/cards/BlueCard";
 import FindEmployee from "./findEmployee";
-import SearchResult from "@/app/components/cards/SearchResult";
+import { SearchResult, ResultsDiv } from "@/app/components/cards/SearchResult";
 
-export default function EmployeeSearch(){
+export default function EmployeeSearch()
+{
   const [searchResults, setSearchResults] = useState<any>([]);
-  const [employeeId, setEmployeeId] = useState<any>();
-  const [name, setName] = useState<String>();
-  const [resultCount, setResultCount] = useState<any>()
+  const [displayList, setDisplayList] = useState<any>([]);
+  const [showResults, setShowResults] = useState<Boolean>(false);
+  let resultList: Array<any> = [];
 
   useEffect(()=>{
-    if(searchResults){
-      setEmployeeId(searchResults.id);
-      let fullName = searchResults.lastName + ', ' + searchResults.firstName;
-      setName(fullName);
-      setResultCount(searchResults.length);
+    if(searchResults&&searchResults.length){
+      searchResults.forEach((result: any) => {
+        let lastName = result.lastName;
+        let firstName = result.firstName;
+        let id = result.id;
+        let branch = result.branch;
+        let url = `http://localhost:3000/employee/${id}`;
+
+        resultList.push(
+          <SearchResult
+            id={id}
+            lastName={lastName}
+            firstName={firstName}
+            branch={branch}
+            url={url}
+          />
+        );
+      });
     }
   },[searchResults]);
 
+  useEffect(()=>{
+    if(resultList&&resultList.length){
+      setDisplayList(resultList);
+      console.log(resultList);
+    }
+  },[resultList]);
+
+  useEffect(()=>{
+    if(displayList&&displayList.length){
+      setShowResults(true);
+    }
+  },[displayList])
+
   return(
     <>
-    <BlueCard content={
-      <div>
-        <h3 className="font-bold">Employee Search</h3>
-      </div>
-    }/>
-    <BlueCard content={
-      <div>
-        <form onSubmit={(e)=>FindEmployee(e, setSearchResults)}>
-          <input className="m-1 p-1" type="text" name="firstName" placeholder="First Name"/>
-          <input className="m-1 p-1" type="text" name="lastName" placeholder="Last Name"/>
-          <input className="m-1 p-1" type="text" name="employeeId" placeholder="Employee ID"/>
-          {/* <input className="m-1 p-1" type="text" placeholder="Assignment ID"/> */}
-          <button className="m-2 mt-8 p-1 pl-3 pr-3 rounded bg-sky-950 text-white flex" type="submit">Submit</button>
-        </form>
-      </div>
-    }/>
-    {searchResults.length&&<BlueCard content={
-      <SearchResult 
-        id={employeeId}
-        name={name}
-        url={`https://localhost:7151/api/Employees/${employeeId}`}
-        count={resultCount}
-      />
-    }/>}
+      <BlueCard content={
+        <div>
+          <h3 className="font-bold">Employee Search</h3>
+        </div>
+      }/>
+
+      <BlueCard content={
+        <div>
+          <form onSubmit={(e)=>FindEmployee(e, setSearchResults)}>
+            <input className="m-1 p-1" type="text" name="firstName" placeholder="First Name"/>
+            <input className="m-1 p-1" type="text" name="lastName" placeholder="Last Name"/>
+            <input className="m-1 p-1" type="text" name="employeeId" placeholder="Employee ID"/>
+            {/* <input className="m-1 p-1" type="text" placeholder="Assignment ID"/> */}
+            <button className="m-2 mt-8 p-1 pl-3 pr-3 rounded bg-sky-950 text-white flex" type="submit">Submit</button>
+          </form>
+        </div>
+      }/>
+      {showResults&&<ResultsDiv searchResultList={displayList}/>}
     </>
   )
 }
