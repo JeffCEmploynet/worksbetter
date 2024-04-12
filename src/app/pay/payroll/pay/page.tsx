ed5fce5d4c9892d0a9910d4587273907f6ab7cff
JeffCEmploynet <jcampbell@employnet.com>
2024-04-12 16:01:30 -0700

    added notes for pay page

--- src/app/pay/payroll/pay/page.tsx
+++ src/app/pay/payroll/pay/page.tsx
@@ -1,6 +1,11 @@
 import BlueCard from "@/app/components/cards/BlueCard"
 
 export default function Pay(){
+  // display session id's and count of timecards for each id
+  // allow for selection of session to pay out
+  // on selection, calculate payment
+    // figure out if billing should be calculated here, or if it should only be calculated from billing tab
+
   return(
     <BlueCard content={
       <h3>Pay them peeps!</h3>
