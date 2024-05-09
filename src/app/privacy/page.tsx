9782ef7cad13b18374fd51fa4cb96d2d256e451c
JeffCEmploynet <jcampbell@employnet.com>
2024-05-09 15:51:42 -0700

    udpated styling on privacy policy

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -4,7 +4,7 @@ export default function Privacy(){
   return (
     <BlueCard content={
     <div className="p-b-2">
-      <h2>Privacy Policy</h2>
+      <h2 className="font-bold text-2xl mb-2">Privacy Policy</h2>
       <p>
           This Privacy Policy applies to (1) job candidates and recipients of Employnet Workforce Management (EWM) services; (2) EWM associates who are sourced or placed on assignment with an EWM client; (3) independent contractor candidates identified by EWM clients who may access this website; (4) users of the EWM website; (5) representatives of EWM business partners, clients, and vendors. 
       </p>
@@ -18,7 +18,7 @@ export default function Privacy(){
       <p>
           If you have questions, please contact EWM by e-mail at hr@employnet.com, by phone at 1-877-EWM-TEMP, or by mail at Employnet Workforce Management, Inc., 2555 Garden Road, Suite H, Monterey, California, 93940
       </p>
-      <p className="bold">
+      <p className="font-bold mt-2">
           Information Employnet Workforce Management, Inc. Collects
       </p>
       <p>
@@ -26,11 +26,11 @@ export default function Privacy(){
           When you use the EWM website, the following categories of information are collected either directly from you when you visit our website or from your browser:
       </p>
       <p>All users:</p>
-      <ul>
+      <ul className="list-disc list-inside mb-2">
           <li>Internet activity including, but not limited to, your browsing activity on our website, your IP address, cookies, and geolocation data;</li>
       </ul>
       <p>Job applicants, candidates, employees:</p>
-      <ul>
+      <ul className="list-disc list-inside mb-2">
           <li>Contact information including, but not limited, to name, email, address, phone number;</li>
           <li>Username and password you create when you register on our website;</li>
           <li>Professional or employment related information including, but not limited to, your job search criteria, availability to work, employment and education history, language proficiencies, work-related skills, reference information, compensation expectations, and other information detailed in your resume;</li>
@@ -42,20 +42,20 @@ export default function Privacy(){
           <li>Information collected with your explicit consent and protected against security breaches including bank account information if you work with us and choose to be paid via direct deposit, drug test results, criminal and background check information.</li>
       </ul>
       <p>Customers/Vendors/Independent Contractors:</p>
-      <ul>
+      <ul className="list-disc list-inside mb-2">
           <li>Contact information including, but not limited to, name, business phone number, and work email;</li>
           <li>Company name, department, and job title;</li>
           <li>Information protected against security breaches including bank and payment information depending on how the customer chooses to pay for services provided.</li>
       </ul>
       <p>Prospective Customers:</p>
-      <ul>
+      <ul className="list-disc list-inside mb-2">
           <li>Contact information including, but not limited to, name, business phone number, work email;</li>
           <li>Company name, department, and job title.</li>
       </ul>
       <p>If you provide your mobile phone number to us, you are opting in and consenting to receive text messages from us. If you do not wish to receive text messages from us, you should not provide your mobile phone number.</p>
-      <p className="bold">Use of Your Personal Information</p>
+      <p className="font-bold mt-2">Use of Your Personal Information</p>
       <p>EWM may disclose the following types of personal information to third parties for business and commercial purposes:</p>
-      <ul>
+      <ul className="list-disc list-inside mb-2">
           <li>Identifiers such as name and email address;</li>
           <li>Internet or other electronic activity information such as browsing history on the EWM website, IP address, search history on the EWM website, your interaction with an advertisement;</li>
           <li>Information like direct deposit or bank account information, which is protected against security breaches, so that workers can be paid, and customers can pay EWM;</li>
@@ -81,7 +81,7 @@ export default function Privacy(){
       <p>
           Business transfers: In the event of a reorganization, merger, sale, joint venture, assignment, transfer, or other disposition of EWM's business (including in connection with any bankruptcy or similar proceeding), EWM may disclose or transfer information to a third party.
       </p>
-      <p className="bold">Retaining Your Information</p>
+      <p className="font-bold mt-2">Retaining Your Information</p>
       <p>
           We will retain your personal information for as long as is necessary in connection with our agreement with you to provide you with the services requested or for other necessary purposes such as compliance with a law, regulation, or other legal authority, or as is otherwise permitted to be maintained for other legal purposes, such as audit, security, fraud prevention, or preserving and defending EWM's legal rights.<br />
           Additionally, job applicants and individuals whose assignment has ended will be maintained in EWM's pool of associates to be potentially contacted about other employment opportunities. Information about individuals will be retained in compliance with applicable legal requirements. Where applicable, EWM will remove your information upon verified request.<br />
@@ -90,7 +90,7 @@ export default function Privacy(){
       <p>
       EWM respects your right to access and control your information. You may exercise specific rights when authorized by applicable law as set forth below:
       </p>
-      <ol>
+      <ol className="list-decimal list-inside mb-2">
           <li>Access: You may access your personal data to verify that it is processed in accordance with the law.</li>
           <li>Correct: You may request the correction of any inaccurate or incomplete data retained about you to protect the accuracy of the information.</li>
           <li>Erase: You may request that information about you be erased and no longer processed.</li>
@@ -120,7 +120,7 @@ export default function Privacy(){
       Because processing your information is necessary to the performance of recruitment, placement, and employment or outsourcing services that EWM provides to you, a request to erase your information may result in EWM being unable to continue providing its services such as recruitment, placement, employment, or outsourcing services. Subject to applicable law, EWM may limit or deny requests or charge reasonable fees.<br />
       Note that your browser settings may allow you to automatically transmit a “Do Not Track” signal to websites and online services you visit. There is no consensus among industry participants as to what “Do Not Track” means in this context. Like many websites and online services, EWM currently does not alter its practices when it receives a “Do Not Track” signal from a visitor's browser. To find out more about “Do Not Track,” you may wish to visit http://www.allaboutdnt.com.
       </p>
-      <p className="bold">
+      <p className="font-bold mt-2">
       Other Privacy Rights
       </p>
       <p>
@@ -138,16 +138,16 @@ export default function Privacy(){
       <p>
       You can opt-out of receiving direct marketing from us at any time. Each marketing communication will provide instructions on how to opt out of receiving similar emails from the registered service or resource. You can also opt-out by contacting the EWM Information Officer at ____________________________.
       </p>
-      <p className="bold">
+      <p className="font-bold mt-2">
       EWM Cookie Usage Policy
       </p>
       <p>
       A cookie is a piece of data stored on the user's computer or mobile device. Cookies allow us, among other things, to store your preferences and settings, enable you to sign-in, and analyze how our website and services are performing. They allow us to identify those devices when they return to a site, facilitating your use of EWM's website, and/or to aid EWM in improving the user experience. You can set your browser to notify you before you receive a cookie, giving you the option of whether to accept it. You can also set your browser to turn off cookies. If you do so, areas of some sites may not function properly.
       </p>
-      <h3>
+      <h3 className="font-bold mt-2 text-xl">
       Types of Cookies Used
       </h3>
-      <h4>
+      <h4 className="font-bold mt-2 text-lg">
       Essential Cookies
       </h4>
       <p>
@@ -156,14 +156,14 @@ export default function Privacy(){
       <p>
       You can set your browser to block or alert you about these cookies, but some parts of the site will not work if they are blocked.
       </p>
-      <h4>
+      <h4 className="font-bold text-lg">
       You can set your browser to block or alert you about these cookies, but some parts of the site will not work if they are blocked.
       </h4>
       <p>
       Performance Cookies allow EWM to measure and understand how the EWM site is used by visitors by identifying which pages are the most and least popular and where we can improve the performance of our site. <br />
       All information these cookies collect is aggregated and therefore anonymous. If you do not allow these cookies, we will not know when you have visited our site and will not be able to monitor its performance.
       </p>
-      <h4>
+      <h4 className="font-bold mt-2 text-lg">
       Functionality Cookies
       </h4>
       <p>
@@ -172,7 +172,7 @@ export default function Privacy(){
       <p>
       These cookies may also collect the user's location-based information to match the user job search criteria on our site. This information is only shared with the mapping provider for the sole purpose of providing the user with the location-based job search service. The user may turn off any geo-locating tracking at the device level. By turning off the location tracking, any geographically based services on our site will no longer be available to you.
       </p>
-      <h4>
+      <h4 className="font-bold mt-2 text-lg">
       Third-Party Cookies
       </h4>
       <p>
@@ -193,7 +193,7 @@ export default function Privacy(){
       <p>
       On portions of EWM's pages, cookies can be controlled by checking or unchecking your consent. Additionally, you can control cookies from your browser-level settings. For additional questions regarding any specific browser, please contact your browser support directly.
       </p>
-      <h3>
+      <h3 className="font-bold mt-2 text-xl">
       Protecting Your Information
       </h3>
       <p>
@@ -208,7 +208,7 @@ export default function Privacy(){
       <p>
       Passwords must be kept confidential and not disclosed to a third party.
       </p>
-      <p className="bold">
+      <p className="font-bold">
       Updates to this Privacy Policy
       </p>
       <p>
@@ -217,7 +217,7 @@ export default function Privacy(){
       <p>
       If we change our Privacy Policy, we will notify you by posting a prominent notice on this site.
       </p>
-      <p className="bold">
+      <p className="font-bold">
       How to Contact Employnet Workforce Management, Inc.
       </p>
       <p>
