a25fb62d4c303d415057a01e442650251be5ff99
JeffCEmploynet <jcampbell@employnet.com>
2024-05-10 09:43:24 -0700

    added billing buttons on page for invoicing and register

--- src/app/bill/page.tsx
+++ src/app/bill/page.tsx
@@ -1,12 +1,18 @@
-
-import BlueCard from "@/app/components/cards/BlueCard"
+import BlueCard from "@/app/components/cards/BlueCard";
+import TitledLinkButton from "../components/buttons/TitledLinkButton";
+import { FaFileInvoiceDollar } from "react-icons/fa6";
+import { LiaFileInvoiceDollarSolid } from "react-icons/lia";
 
 export default function Bill(){
 
   return(
     <BlueCard content={
-      <div>
-        <h3>Welcome! You have reached the Bill Page!</h3>
+      <div className="flex flex-col">
+        <h3 className="p-1 rounded m-1 font-bold text-lg items-center text-sky-950">Billing</h3>
+        <div className="flex flex-row w-full justify-between">
+          <TitledLinkButton linkRef="/bill/invoicing" linkName="Invoicing" linkIcon={<FaFileInvoiceDollar />}/>
+          <TitledLinkButton linkRef="/bill/invoice-register" linkName="Invoice Register" linkIcon={<LiaFileInvoiceDollarSolid />}/>
+        </div>
       </div>
     }/>
   )
